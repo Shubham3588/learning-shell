68802e9d8ca7194565cfce9e87a720b3f95ee37b
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2022-09-17 06:04:43 +0530

    frontend code is added

--- learning/02-print.sh
+++ learning/02-print.sh
@@ -12,8 +12,26 @@ echo Hello World
 echo -e "Hello,\nHow are you doing?\nGood Day"
 
 #Print a tab space using \t
-
 echo -e "a\tb"
+
+#Print some colours
+#Syntax: echo -e "\e[COLmMESSAGE\e[0m"
+# -e -> enable esc sequence
+# \e -> enable col
+# [COLm -> Give color input
+# \e[0m -> Disable the color
+
+#What are the colors we have?
+#COL               Code            Syntax
+#RED                 31             \e[31m
+#Green               32              \e[32m
+#yellow              33              \e[33m
+#Blue                34               \e[34m
+#Magneta             35               \e[35m
+#CYAN                36                \e[36m
+
+echo "\e[31mPrint in red Color\e[0m"
+echo "Print Normal text"
 
 
 
